3a718b19aadb119d2353f3298bd957a28512564a
Luis Cosio <luisalfonsocosioizcapa@gmail.com>
2025-02-10 16:19:11 -0600

    SEO optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.amias.mx",
-  integrations: [tailwind({ configFile: "./tailwind.config.mjs" })],
+  integrations: [tailwind({ configFile: "./tailwind.config.mjs" }), sitemap()],
 });
